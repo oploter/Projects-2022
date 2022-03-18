83d2421029daf1401585b7301032c981318600b6
oploter <90156235+oploter@users.noreply.github.com>
2022-03-18 10:35:47 +0300

    Create server.h

---
+++ server.h
@@ -0,0 +1,22 @@
+#ifndef SERVER_H 
+#define SERVER_H 
+#include "server_map.h" 
+#include "declarations.h" 
+#include <utility> 
+#include "cl_sr_queue.h" 
+struct ServerGame{ 
+private: 
+    ServerMap map; 
+    sf::TcpListener listener; 
+    sf::TcpSocket socket; 
+    sf::Packet packet; 
+    threadsafe_queue<sf::Packet> Q; 
+    float x, y; 
+     
+public: 
+    void runGameServer(); 
+    void SendMessages(); 
+    void ReceiveMessages(); 
+    //void checkAndPlant(); 
+}; 
+#endif
